2c83b26f1426d0a7ebf70cb5d4907a7446d0e001
Olvine23 <olivinegeorge@gmail.com>
2024-10-19 21:15:28 +0300

    Co-authored-by: Vanessa3456 <Vanessa3456@users.noreply.github.com>

---
+++ lib/config/colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+ final  backgroundColor = const Color(0xff684e37);
+
+ final secondaryColor =  const Color(0xffdbd6d0);
+
+
+final accentColor1 = const Color(0xffdfef6a);
+
+final accentColor2 = const Color(0xff4e574e);
+
+final accentColor3 = const Color(0xffffd89b); //romantic container
+
+final accentColor4 = const Color(0xfff3f9c8); //non-fiction container
+
+
+
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,14 @@
+import 'package:bookfuse/config/colors.dart';
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors().backgroundColor
+      
+    );
+  }
+}
